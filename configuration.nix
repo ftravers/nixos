f02d50141450286f5b817717cef94cb6b64ea8e3
Fenton Travers <fenton.travers@gmail.com>
2017-08-31 08:54:48 -0700

    set default shell to zsh

--- configuration.nix
+++ configuration.nix
@@ -1,14 +1,12 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
+
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
+
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
@@ -19,8 +17,10 @@
   # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
+
   networking.hostName = "cpu4desk"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
 
   # Select internationalisation properties.
   i18n = {
@@ -29,23 +29,22 @@
     defaultLocale = "en_US.UTF-8";
   };
 
+
   # Set your time zone.
   time.timeZone = "America/Vancouver";
+
 
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-    wget
-    vim
-    emacs
-    git
-    mkpasswd
-  ];
+    wget vim emacs git mkpasswd zsh
+    ];
+
 
   # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -53,17 +52,21 @@
   # Or disable the firewall altogether.
   networking.firewall.enable = false;
 
+
   # Enable CUPS to print documents.
   services.printing.enable = true;
+
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.xserver.layout = "us";
   services.xserver.xkbOptions = "eurosign:e";
 
+
   # Enable the KDE Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.fenton = {
@@ -78,8 +81,9 @@
     openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDAG5wvPKRrqqiOBKZ5dUqF5oK7vd1zNuVsrrAAQCcEkVC2SBXVy5yiCiO7xPz7Wk6oSl+5nvkitYQ4HVuNO+mroUcmbge/e344sfyOytrV2BqFTuijlc+BkBTMk55piHKBgl50l4gIdtTdKk1b0iiTxc5gdhlUr4LUF+mPc5NnuKgMEJLApoFeNKrzbR+Z5ZsLypeFNxzkaAw8mjqRoDoi1lab7tDN/KrVKZ46AYXm9Tix64MdxXI6T+p6Z+2rAQQ0ieexVtVUJBiifaKrvqgr57v8WPFk8VIYb9MbtlxtHdHz/regzZA4L6K+46QpSFeBX29esx1/tuihv/hU8ndf fenton@ss9" ];
 };
 
+  users.defaultUserShell = "/var/run/current-system/sw/bin/zsh";
+  users.mutableUsers = false;
 
-users.mutableUsers = false;
 
   # The NixOS release to be compatible with for stateful data such as databases.
   system.stateVersion = "17.03";
